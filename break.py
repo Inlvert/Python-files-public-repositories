bcf1392b0a757e5fc02cea0c7892b37994c21705
Inlvert <inlvert@gmail.com>
2021-06-01 16:18:18 +0300

    commit

---
+++ break.py
@@ -0,0 +1,6 @@
+while True:
+    s = input("write samthing : ")
+    if s == "Выход":
+        break
+    print("Line length:", len(s))
+print("The and")
---
+++ for.py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+    print(i)
+else:
+    print("Cycle 'for' is over")
+
+for i in range(1,11,2):
+    print(i)
+else:
+    print("Cycle 'for' is over")
