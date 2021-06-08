c9bd2acf30f19fc81e0425bf3613c2e6974803bc
Inlvert <inlvert@gmail.com>
2021-06-08 14:02:45 +0300

    commit my

---
+++ continue.py
@@ -0,0 +1,9 @@
+while True:
+    s = input("write samthing : ")
+    if s == "Выход":
+        break
+    if len(s) < 3:
+        print("Слишком мало")
+        continue
+    print("Введённая строка достаточной длинны")
+    # Разные другие действия здесь...
